de0a3f62001abda105d9d40cbfbf2e3fc13c7acc
Terrance Tran <ttran136@wch136-30.cs.ucr.edu>
2020-04-15 16:35:38 -0700

    lab2 part3

---
+++ Lab2_introToAvr/turnin/ttran136_lab2_part3.c
@@ -0,0 +1,36 @@
+/*	Author: ttran136
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+
+	unsigned char cntavail = 0x00;
+    /* Insert your solution below */
+	while(1){
+	cntavail = ((PINA & 0x08)>>3)+
+		((PINA & 0x04)>>2)+
+		((PINA & 0x02)>>1)+
+		((PINA & 0x01));
+	if(cntavail == 0x04){
+		PORTC = 0x80;
+	}
+	else{
+		cntavail = 0x04 - cntavail;
+		PORTC = cntavail;
+	}
+	return 1;
+}
+}
