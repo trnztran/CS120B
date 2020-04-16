3bc958aa0f83e326e53d59f8a399c962502165d6
Terrance Tran <ttran136@wch136-30.cs.ucr.edu>
2020-04-15 20:01:53 -0700

    lab3 part1

---
+++ Lab3/turnin/ttran136_lab3_part1.c
@@ -0,0 +1,44 @@
+/*	Author: ttran136
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0x00; PORTC = 0xFF;
+	
+	unsigned char numA = 0x00;
+	unsigned char numB = 0x00;
+	unsigned char totalA = 0x00;
+	unsigned char totalB = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	numA = PINA;
+	numB = PINB;
+	totalA = 0x00;
+	totalB = 0x00;
+	while(numA || numB){
+		if(numA & 0x01){
+			totalA++;
+		}
+		if(numB & 0x01){
+			totalB++;
+		}
+		numA = numA>>1;
+		numB = numB>>1;
+	}
+	PORTC = totalA +totalB;
+}
+	return 1;
+}
