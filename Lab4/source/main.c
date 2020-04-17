8b0fd3dfef191708f0284d1c03970a21eb23e95d
Terrance Tran <ttran136@wch136-30.cs.ucr.edu>
2020-04-17 15:56:23 -0700

    Lab4 part1

---
+++ Lab4/source/main.c
@@ -0,0 +1,64 @@
+/*	Author: ttran136
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, Init, ButtonPress} State;
+unsigned char button = 0x00;
+
+void Led(){
+	switch(State){
+		case Start:
+			State = Init;
+			break;
+		
+		case Init:
+			if(button== 0x01){
+				State = ButtonPress;
+			}
+			else if( button == 0x00){
+				State = Init;
+			}
+			break;
+		case ButtonPress:
+			if(button == 0x01){
+				State = ButtonPress;
+			}
+			else if(button == 0x00){
+				State = Init;
+			}
+			break;
+		default: State = Start; 
+			break;
+	}
+	switch(State){
+		case Init:
+			PORTB = 0x01;
+			break;
+		case ButtonPress:
+			PORTB = 0x02;
+			break;
+		default: break;
+	} 
+}	
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+    /* Insert your solution below */
+    State = Start;
+	while (1) {
+		button = PINA & 0x01;
+		Led();
+    }
+    return 1;
+}
