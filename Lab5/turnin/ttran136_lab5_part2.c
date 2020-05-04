8a0b99cffb359dfda6ca75104f6499fc9fda3bc9
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-03 22:44:08 -0700

    Update ttran136_lab5_part2.c

--- Lab5/turnin/ttran136_lab5_part2.c
+++ Lab5/turnin/ttran136_lab5_part2.c
@@ -12,12 +12,110 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States{Start, init, wait, increment, decrement, reset, wait1, wait2, wait3}state;
+unsigned char buttonA0 = 0x00;
+unsigned char buttonA1 = 0x00;
+unsigned char tmpC = 0x00;
+
+void tick(){
+    switch(state){
+        case Start:
+            state = init;
+            break;
+        case init:
+            state = wait;
+            break;
+        case wait:
+            if(!buttonA0 && !buttonA1){
+                state = wait;
+            }
+            else if(buttonA0 && buttonA1){
+                state = reset;
+            }
+            else if((tmpC < 9) && (buttonA0 && !buttonA1)){
+                state = increment;
+            }
+            else if((tmpC > 0) && (!buttonA0 && buttonA1)){
+                state = decrement;
+            }
+            else{
+                state = wait;
+            }
+            break;
+        case increment:
+            state = wait1;
+            break;
+        case decrement:
+            state = wait2;
+            break;
+        case reset:
+            state = wait3;
+            break;
+        case wait3:
+            if(!buttonA0 && !buttonA1){
+                state = wait;
+            }
+            else if(buttonA0 && buttonA1){
+                state = wait3;
+            }
+            break;
+        case wait2:
+            if(buttonA0 && buttonA1){
+                state = reset;
+            }
+            else if (!buttonA1){
+                state = wait2;
+            }
+            break;
+        case wait1:
+            if(buttonA0 && buttonA1){
+                state = reset;
+            }
+            else if (buttonA0){
+                state = wait1;
+            }
+            break;
+        default:
+            break;
+    }
+    switch(state){
+        case Start:
+            break;
+        case init:
+            tmpC = 0x00;
+            break;
+        case wait:
+            break;
+        case increment:
+            tmpC++;
+            break;
+        case decrement:
+            tmpC--;
+            break;
+        case reset:
+            tmpC = 0x00;
+            break;
+        case wait3:
+        case wait2:
+        case wait1:
+            break;
+        default:
+            break;
+    }
+}
+            
+
 int main(void) {
     /* Insert DDR and PORT initializations */
- 	DDRA;
+ 	DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
     /* Insert your solution below */
-    while (1) {
 
+    while (1) {
+            PORTC = tmpC;
+            buttonA0 = ~PINA & 0x01;
+            buttonA1 = ~PINA & 0x02;
+            tick();
     }
     return 1;
 }
