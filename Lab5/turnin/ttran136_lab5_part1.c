d4534b23aa573db4e27713aa28fc090e18b58557
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-03 22:25:49 -0700

    Update ttran136_lab5_part1.c

--- Lab5/turnin/ttran136_lab5_part1.c
+++ Lab5/turnin/ttran136_lab5_part1.c
@@ -14,10 +14,45 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
- 	DDRA;
+ 	DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    unsigned char FuelLevel = 0x00;
+    unsigned char sensor = 0x00;
     /* Insert your solution below */
     while (1) {
-
+        sensor = ~PINA;
+        switch(sensor & 0xF){
+            case 1:
+            case 2:FuelLevel = 0x60;
+                break;
+            case 3:
+            case 4:
+                FuelLevel = 0x70;
+                break;
+            case 5:
+            case 6:
+                FuelLevel = 0x38;
+                break;
+            case 7:
+            case 8:
+            case 9:
+                FuelLevel = 0x3C;
+                break;
+            case 10:
+            case 11:
+            case 12:
+                FuelLevel = 0x3E;
+                break;
+            case 13:
+            case 14:
+            case 15:
+                FuelLevel = 0x3F;
+                break;
+            default:
+                FuelLevel = 0x00;
+                break;
+        }
+        PORTC = FuelLevel;
     }
     return 1;
 }
