d5c312a7d577cc056d001ab49785e52acf1ab31d
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-10 22:33:02 -0700

    Update ttran136_lab6_part1.c

--- Lab_6/turnin/ttran136_lab6_part1.c
+++ Lab_6/turnin/ttran136_lab6_part1.c
@@ -8,16 +8,104 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+volatile unsigned char TimerFlag=0;
+unsigned long _avr_timer_M=1;
+unsigned long _avr_timer_cntcurr=0;
+ 
+void TimerISR()
+{
+	TimerFlag=1;
+}
 
-    /* Insert your solution below */
-    while (1) {
+void TimerSet(unsigned long M)
+{
+	_avr_timer_M=M;
+	_avr_timer_cntcurr=_avr_timer_M;
+}
 
+void TimerOn()
+{
+	TCCR1B=0x0B;
+	OCR1A=125;
+	TIMSK1=0x02;
+	TCNT1=0;
+	_avr_timer_cntcurr=_avr_timer_M;
+	SREG |= 0x80;
+}
+
+void TimerOff()
+{
+	TCCR1B=0x00;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr==0)
+	{
+		TimerISR();
+		_avr_timer_cntcurr=_avr_timer_M;
+	}
+}
+
+enum states{Start,led1,led2,led3}STATES;
+	
+void tick()
+{
+	switch(STATES) //Transition
+	{
+		case Start:
+		STATES=led1;
+		break;
+		
+		case led1:
+		STATES=led2;
+		break;
+		
+		case led2:
+		STATES=led3;
+		break;
+		
+		case led3:
+		STATES=led1;
+		break;
+		
+		default:
+		STATES=Start;
+		break;
+	}
+	
+	switch(STATES) //Actions
+	{
+		case led1:
+		PORTB=0x01;
+		break;
+		
+		case led2:
+		PORTB=0x02;
+		break;
+		
+		case led3:
+		PORTB=0x04;
+		break;
+		
+		default:
+		break;
+	}
+}
+
+
+int main(void)
+{
+	DDRB=0xFF; PORTB=0x00;
+	TimerSet(1000);
+	TimerOn();
+	
+    while (1) 
+    {
+		tick();
+		while(!TimerFlag);
+		TimerFlag=0;
     }
-    return 1;
 }
