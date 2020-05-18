7ca25641531f245a29ca3558b8cef0b0dd6f0b03
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-17 23:31:14 -0700

    Update ttran136_lab8_part2.c

--- Lab_8/turnin/ttran136_lab8_part2.c
+++ Lab_8/turnin/ttran136_lab8_part2.c
@@ -12,12 +12,23 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
 
-    /* Insert your solution below */
-    while (1) {
+unsigned short my_short = 0x00;
+	
+int main(void)
+{	
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	ADC_init();
 
+    while(1)
+    {
+			my_short = ADC;
+			PORTB =  (char)my_short;
+			PORTD = (char)(my_short >> 8);
     }
-    return 1;
 }
