83862e5038e94d421e44b523de67673a3bc9f062
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-24 02:09:55 -0700

    Update ttran136_lab10_part2.c

--- Lab_10/turnin/ttran136_lab10_part2.c
+++ Lab_10/turnin/ttran136_lab10_part2.c
@@ -11,13 +11,125 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+#include "io.h"
+#include "timer.h"
+
+enum states1{start1,led1,led2,led3}state1;
+enum states2{start2, blinkOn, blinkOff}state2;
+enum states3{start3, combSM}state3;
+static unsigned char threeLEDs;
+static unsigned char blinkingLED;
+
+void tick1(){
+    switch(state1){
+        case start1:
+            state1 = led1;
+            break;
+        case led1:
+            state1 = led2;
+            break;
+        case led2:
+            state1 = led3;
+            break;
+        case led3:
+            state1 = led1;
+            break;
+        default:
+            break;
+    }
+    switch(state1){
+        case start1:
+            break;
+        case led1:
+            threeLEDs = 0x01;
+            break;
+        case led2:
+            threeLEDs = 0x02;
+            break;
+        case led3:
+            threeLEDs = 0x04;
+            break;
+        default:
+            break;
+    }
+}
+
+void tick2(){
+    switch(state2){
+        case start2:
+            state2 = blinkOn;
+            break;
+        case blinkOn:
+            state2 = blinkOff;
+            break;
+        case blinkOff:
+            state2 = blinkOn;
+            break;
+        default:
+            break;
+    }
+    switch(state2){
+        case start2:
+            break;
+        case blinkOn:
+            blinkingLED = 0x08;
+            break;
+        case blinkOff:
+            blinkingLED = 0x00;
+            break;
+        default:
+            break;
+    }
+}
+
+void tick3(){
+    switch(state3){
+        case start3:
+            state3 = combSM;
+            break;
+        case combSM:
+            break;
+        default:
+            break;
+    }
+    switch(state3){
+        case start3:
+            break;
+        case combSM:
+            PORTB = (threeLEDs|blinkingLED);
+            break;
+        default:
+            break;
+    }
+}
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRB = 0xFF; PORTB = 0x00;
+    unsigned long threePeriod = 300;
+    unsigned long blinkPeriod = 1000;
+    unsigned long PERIOD = 100;
+    TimerSet(PERIOD);
+    TimerOn();
+    state1 = start1;
+    state2 = start2;
+    state3 = start3;
     /* Insert your solution below */
     while (1) {
-
+        if(threePeriod >= 300){
+            tick1();
+            threePeriod = 0;
+        }
+        if(blinkPeriod >= 1000){
+            tick2();
+            blinkPeriod = 0;
+        }
+        tick3();
+        while(!TimerFlag){
+        }
+        TimerFlag = 0;
+        threePeriod += PERIOD;
+        blinkPeriod += PERIOD;
     }
     return 1;
 }
