05aa8ed62145cb009b4735e605e4ca22e7e38028
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-27 12:55:15 -0700

    Update detect_eq.h

---
+++ Midterm/header/detect_eq.h
@@ -0,0 +1,19 @@
+void Detect_EQ()
+{
+    switch(detect_eq_state)
+    {
+        case DEQInit:
+            //init variable(s) here.
+            break;
+        default:
+            detect_eq_state = DEQInit;
+            break;
+    }
+    switch(detect_eq_state)
+    {
+        case DEQInit:
+            break;
+        default:
+            break;
+    }
+}
