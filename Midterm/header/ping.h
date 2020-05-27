372524710a2e492df4af6fc41416bbf68349a366
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-27 13:07:27 -0700

    Update ping.h

--- Midterm/header/ping.h
+++ Midterm/header/ping.h
@@ -3,6 +3,13 @@ void Ping()
     switch(ping_state)
     {
         case PInit:
+            ping_state = pingLow;
+            break;
+        case pingLow:
+            ping_state = pingHigh;
+            break;
+        case pingHigh:
+            ping_state = pingLow;
             break;
         default:
             ping_state = PInit;
@@ -11,6 +18,12 @@ void Ping()
     switch(ping_state)
     {
         case PInit:
+            break;
+        case pingLow:
+            pingB = 0x00;
+            break;
+        case pingHigh:
+            pingB = 0x01;
             break;
         default:
             break;
