f9456404993a5506c728260f5143f13a01d18a83
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-27 12:56:05 -0700

    Update detect_zc.h

---
+++ Midterm/header/detect_zc.h
@@ -0,0 +1,19 @@
+void Detect_ZC()
+{
+    switch(detect_zc_state)
+    {
+        case DZCInit:
+            break;
+        
+        default:
+            detect_zc_state = DZCInit;
+            break;
+    }
+    switch(detect_zc_state)
+    {
+        case DZCInit:
+            break;
+        default:
+            break;
+    }
+}
