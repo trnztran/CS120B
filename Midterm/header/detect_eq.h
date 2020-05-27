367af33a8a46c91f772f45ea46c0b12c678a61b5
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-27 13:04:57 -0700

    Update detect_eq.h

--- Midterm/header/detect_eq.h
+++ Midterm/header/detect_eq.h
@@ -3,8 +3,23 @@ void Detect_EQ()
     switch(detect_eq_state)
     {
         case DEQInit:
-            //init variable(s) here.
+            if(amplitude = 0){
+                detect_eq_state = DEQInit;
+            }
+            else if(amplitude > 0){
+                detect_eq_statq = eq_detected;
+            }
+            else{
+                detect_eq_state = DEQInitl
+            }
             break;
+        case eq_detected:
+            if(amplitude <= 0){
+                detect_eq_state = DEQInit;
+            }
+            else{
+                detect_eq_state = DEQInit;
+            }
         default:
             detect_eq_state = DEQInit;
             break;
@@ -12,6 +27,10 @@ void Detect_EQ()
     switch(detect_eq_state)
     {
         case DEQInit:
+            eq = 0x00;
+            break;
+        case eq_detected:
+            eq = 0x02;
             break;
         default:
             break;
