90d393f422a6ae60d96926d56cb70f26ddf1a7cf
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-27 12:56:28 -0700

    Update ping.h

---
+++ Midterm/header/ping.h
@@ -0,0 +1,18 @@
+void Ping()
+{
+    switch(ping_state)
+    {
+        case PInit:
+            break;
+        default:
+            ping_state = PInit;
+            break;
+    }
+    switch(ping_state)
+    {
+        case PInit:
+            break;
+        default:
+            break;
+    }
+}
