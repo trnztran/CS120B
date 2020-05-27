f4db294b7b9b942bb232f872ea0045807a6e7a0e
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-27 12:55:39 -0700

    Update detect_max_amp.h

---
+++ Midterm/header/detect_max_amp.h
@@ -0,0 +1,18 @@
+void Detect_Max_Amp()
+{
+    switch(detect_max_amp_state)
+    {
+        case DMAInit:
+            break;
+        
+        default:
+            break;
+    }
+    switch(detect_max_amp_state)
+    {
+        case DMAInit:
+            break;
+        default:
+            break;
+    }
+}
