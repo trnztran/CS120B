520bdfc3cb946781e0395374ab181d82adf053d7
trnztran <59709057+trnztran@users.noreply.github.com>
2020-05-27 12:57:26 -0700

    Update transmit.h

---
+++ Midterm/header/transmit.h
@@ -0,0 +1,19 @@
+void Transmit()
+{
+    switch(transmit_state)
+    {
+        case TInit:
+            break;
+        default:
+            transmit_state = TInit;
+            break;
+    }
+    switch(transmit_state)
+    {
+        case TInit:
+            break;
+        default:
+            break;
+    }
+
+}
