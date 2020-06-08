49701d6a3caa1582ae50da60c6cfb720bd02cc02
trnztran <59709057+trnztran@users.noreply.github.com>
2020-06-07 23:32:48 -0700

    Update ttran136_lab11_part4.c

--- Lab_11/turnin/ttran136_lab11_part4.c
+++ Lab_11/turnin/ttran136_lab11_part4.c
@@ -11,18 +11,21 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+#include "timer.h"
+#include "scheduler.h"
+#include "keypad.h"
+#include "io.h"
+
+enum States{output};
 unsigned char tmpA = 0x00;
 unsigned char cursor = 1;
 
-
-enum States{output};
-
-int SMTick1(int state){
-	unsigned char x;
-	x = GetKeypadKey();
+int tick(int state){
+	unsigned char i;
+	i = GetKeypadKey();
 	switch(state){
 		case output:
-			switch (x) {
+			switch (i) {
 				case '\0': 
 					break; 
 				case '1': 
@@ -138,42 +141,23 @@ int SMTick1(int state){
 
 int main()
 {
-	// Set Data Direction Registers
-	// Buttons PORTA[0-7], set AVR PORTA to pull down logic
 	DDRA = 0xFF; PORTA = 0x00;
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
+	DDRC = 0xF0; PORTC = 0x0F; 
 	DDRD = 0xFF; PORTD = 0x00;
-	// Period for the tasks
-	unsigned long int SMTick1_calc = 50;
 
-
-	//Calculating GCD
-	unsigned long int tmpGCD = 1;
-
-	//Greatest common divisor for all tasks or smallest time unit for tasks.
-	unsigned long int GCD = tmpGCD;
-
-	//Recalculate GCD periods for scheduler
-	unsigned long int SMTick1_period = SMTick1_calc;
-
-	//Declare an array of tasks
 	static task task1;
 	task *tasks[] = { &task1};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
-	// Task 1
 	task1.state = 0;//Task initial state.
-	task1.period = SMTick1_period;//Task Period.
-	task1.elapsedTime = SMTick1_period;//Task current elapsed time.
-	task1.TickFct = &SMTick1;//Function pointer for the tick.
+	task1.period = 15
+	task1.elapsedTime = 15
+	task1.TickFct = &tick;
 
-
-	// Set the timer and turn it on
-	TimerSet(GCD);
+	TimerSet(10);
 	TimerOn();
 	LCD_init();
-	LCD_DisplayString(1,"Congratulations");
+	LCD_DisplayString(1,"Congratulations!");
 
 	unsigned short i; // Scheduler for-loop iterator
 	while(1) {
