b601ebf061ff9ab16ecdd09dfe3c8311f705b756
trnztran <59709057+trnztran@users.noreply.github.com>
2020-06-07 19:27:45 -0700

    Update ttran136_lab11_part4.c

--- Lab_11/turnin/ttran136_lab11_part4.c
+++ Lab_11/turnin/ttran136_lab11_part4.c
@@ -11,13 +11,187 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+unsigned char tmpA = 0x00;
+unsigned char cursor = 1;
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
 
-    /* Insert your solution below */
-    while (1) {
+enum States{output};
 
-    }
-    return 1;
+int SMTick1(int state){
+	unsigned char x;
+	x = GetKeypadKey();
+	switch(state){
+		case output:
+			switch (x) {
+				case '\0': 
+					break; 
+				case '1': 
+					tmpA = 0x01;
+					LCD_Cursor(cursor); 
+					LCD_WriteData(tmpA + '0'); 
+					cursor++; 
+					break; 
+				case '2': 
+					tmpA = 0x02;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + '0'); 
+                    cursor++; 
+                    break;
+				case '3': 
+                    tmpA = 0x03;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + '0'); 
+                    cursor++; 
+                    break;
+				case '4': 
+                    tmpA = 0x04; 
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + '0'); 
+                    cursor++; 
+                    break;
+				case '5': 
+                    tmpA = 0x05;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + '0'); 
+                    cursor++; 
+                    break;
+				case '6': 
+                    tmpA = 0x06;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + '0'); 
+                    cursor++; 
+                    break;
+				case '7': 
+                    tmpA = 0x07;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + '0'); 
+                    cursor++; 
+                    break;
+				case '8': 
+                    tmpA = 0x08;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + '0'); 
+                    cursor++; 
+                    break;
+				case '9': 
+                    tmpA = 0x09;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + '0'); 
+                    cursor++; 
+                    break;
+				case 'A': 
+                    tmpA = 0x0A;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + 0x37); 
+                    cursor++; 
+                    break;
+				case 'B': 
+                    tmpA = 0x0B;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + 0x37); 
+                    cursor++; 
+                    break;
+				case 'C': 
+                    tmpA = 0x0C;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + 0x37); 
+                    cursor++; 
+                    break;
+				case 'D': 
+                    tmpA = 0x0D;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + 0x37); 
+                    cursor++; 
+                    break;
+				case '*': 
+                    tmpA = 0x0E;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + 0x1C); 
+                    cursor++; 
+                    break;
+				case '0': 
+                    tmpA = 0x00;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + '0'); 
+                    cursor++; 
+                    break;
+				case '#': 
+                    tmpA = 0x0F;
+				    LCD_Cursor(cursor); 
+                    LCD_WriteData(tmpA + 0x14); 
+                    cursor++; 
+                    break;
+				default: 
+                    tmpA = 0x1B; 
+                    break; 
+			}
+			if(cursor==17){
+				cursor=1;
+			}
+			state = output;
+			PORTA=tmpA;
+			break;
+		}
+		return state;
+}
+
+
+int main()
+{
+	// Set Data Direction Registers
+	// Buttons PORTA[0-7], set AVR PORTA to pull down logic
+	DDRA = 0xFF; PORTA = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
+	DDRD = 0xFF; PORTD = 0x00;
+	// Period for the tasks
+	unsigned long int SMTick1_calc = 50;
+
+
+	//Calculating GCD
+	unsigned long int tmpGCD = 1;
+
+	//Greatest common divisor for all tasks or smallest time unit for tasks.
+	unsigned long int GCD = tmpGCD;
+
+	//Recalculate GCD periods for scheduler
+	unsigned long int SMTick1_period = SMTick1_calc;
+
+	//Declare an array of tasks
+	static task task1;
+	task *tasks[] = { &task1};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+	// Task 1
+	task1.state = 0;//Task initial state.
+	task1.period = SMTick1_period;//Task Period.
+	task1.elapsedTime = SMTick1_period;//Task current elapsed time.
+	task1.TickFct = &SMTick1;//Function pointer for the tick.
+
+
+	// Set the timer and turn it on
+	TimerSet(GCD);
+	TimerOn();
+	LCD_init();
+	LCD_DisplayString(1,"Congratulations");
+
+	unsigned short i; // Scheduler for-loop iterator
+	while(1) {
+		// Scheduler code
+		for ( i = 0; i < numTasks; i++ ) {
+			// Task is ready to tick
+			if ( tasks[i]->elapsedTime >= tasks[i]->period ) {
+				// Setting next state for task
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				// Reset the elapsed time for next tick.
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += 1;
+		}
+		while(!TimerFlag);
+		TimerFlag = 0;
+	}
+
+	// Error: Program should not exit!
+	return 0;
 }
